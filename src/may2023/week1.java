6a858de03fbc48549ddc326bc41513ef7bc8dade
Lee <18751977178@163.com>
2023-05-04 13:57:01 +0800

    No.1003
    检查替换后的词是否有效

--- src/may2023/week1.java
+++ src/may2023/week1.java
@@ -36,6 +36,22 @@ public class week1 {
         return new ArrayList<>(set);
     }
 
+
+    //1003. 检查替换后的词是否有效
+    public boolean isValid(String s) {
+        StringBuilder stk = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            stk.append(c);
+            if (stk.length() >= 3 && stk.substring(stk.length() - 3).equals("abc")) {
+                stk.delete(stk.length() - 3, stk.length());
+            }
+        }
+        return stk.length() == 0;
+    }
+
+
+
     public static void main(String[] args) {
         System.out.println(powerfulIntegers(1, 1, 10));
     }
