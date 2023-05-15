fd4d7b93163b0dda95ca61a3bb68a1af7f8a11e0
Ningxuan Li <84451818+Lee1003@users.noreply.github.com>
2023-05-15 21:48:14 +0800

    No.1015
    可被 K 整除的最小整数

--- src/may2023/week2.java
+++ src/may2023/week2.java
@@ -1,5 +1,8 @@
 package may2023;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class week2 {
 
     //2437. 有效时间的数目
@@ -42,8 +45,24 @@ public class week2 {
         return count1 * count2;
     }
 
+
+    //1015. 可被 K 整除的最小整数
+    public int smallestRepunitDivByK(int k) {
+        Set<Integer> set = new HashSet<>();
+        int x = 1 % k;
+        while (x > 0 && set.add(x)) {
+            x = (x * 10 + 1) % k;
+        }
+        return x > 0 ? -1 : set.size() + 1;
+    }
+
+
+
+
+
+
+
     public static void main(String[] args) {
-        char c = '1';
-        System.out.println(c - '0');
+        System.out.println(111 % 7);
     }
 }
